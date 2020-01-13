68a6ed2dd0c9a6f1cb0d934370f7dd9a6604045f
ToddLa <ToddLa@HotMail.com>
2020-01-13 15:30:38 -0800

    Initial Commit

---
+++ WordList/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  WordList
+//
+//  Created by Todd Laney on 1/13/20.
+//  Copyright © 2020 Todd Laney. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
